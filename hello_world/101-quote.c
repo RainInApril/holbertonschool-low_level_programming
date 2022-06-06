684ad477d78643f70bab17f2d3e8322498e7df41
root <chaubnphan@gmail.com>
2022-06-06 02:43:46 -0700

    Add an executable c script to file 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always return 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
