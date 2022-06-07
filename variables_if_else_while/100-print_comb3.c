affc6b42ea1f091d4ec6a4c712dba4db11b7760a
root <chaubnphan@gmail.com>
2022-06-07 02:43:01 -0700

    Edit script to print smallest combo

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '8'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
 			putchar(a);
 			putchar(b);
 
-			if (a < '9' || b < '9')
+			if (a < '8' || b < '9')
 			{
 				putchar(',');
 				putchar(' ');
