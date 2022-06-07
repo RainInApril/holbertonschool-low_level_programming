c5d6fa082c19486d6f9133a6d077541ea57ce476
root <chaubnphan@gmail.com>
2022-06-06 22:22:08 -0700

    Add an executable c script to file 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase. except q & e
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
