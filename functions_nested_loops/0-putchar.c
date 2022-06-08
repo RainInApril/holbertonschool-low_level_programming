4a8b5adfae8730281e475e11f2b5247d09854051
root <chaubnphan@gmail.com>
2022-06-07 21:49:10 -0700

    Add an executable c script to file 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	while (:)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory was created for the C - Functions, nested loops project at Holberton Australia
