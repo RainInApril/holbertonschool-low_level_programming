9fa559a2ea0a953f8d450722b16751453d8b1ae7
root <chaubnphan@gmail.com>
2022-06-13 22:49:59 -0700

    Add a function that prints a triangle to file 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int row, space, hash;
+	
+	for (row = 1; row <= size; row++)
+	{
+		for (space = 1; space <= (size - row); space++)
+		{
+			_putchar(' ');
+		}
+		for (hash = 1; hash <= row; hash++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
