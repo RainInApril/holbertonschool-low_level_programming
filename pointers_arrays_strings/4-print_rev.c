e6c117502ca350d87ff6d3d2eaa5fe4ccb58fa75
root <chaubnphan@gmail.com>
2022-06-16 17:14:36 -0700

    Remove c file and modify script

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,28 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
  * print_rev - prints a string, in reverse
  * @s: the string being reversed
  */
 
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
 void print_rev(char *s)
 {
-	int i;
-	int length = _strlen(s) - 1;
-
-	for (i = length; i >= 0; i--)
+	int i = 0;
+	i = _strlen(s) - 1;
+	
+	while (i >= 0)
 	{
 		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
