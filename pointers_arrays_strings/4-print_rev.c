71846a12aceae5be3d842f31f4b801ff5c50c354
root <chaubnphan@gmail.com>
2022-06-16 18:21:03 -0700

    Fix betty errors

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse
- * @s: the string being reversed
+ * _strlen - prints the length of a string
+ * @s: the string being counted for length
+ * Return: the string's length
  */
 
 int _strlen(char *s)
@@ -14,11 +15,16 @@ int _strlen(char *s)
 	return (len);
 }
 
+/**
+ * print_rev - prints a string, inr reverse
+ * @s: the string being reversed
+ */
+
 void print_rev(char *s)
 {
 	int i = 0;
+
 	i = _strlen(s) - 1;
-	
 	while (i >= 0)
 	{
 		_putchar(s[i]);
