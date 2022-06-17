1ff5cdf2cb5cc1c68fb103bc9bec5be095978c06
root <chaubnphan@gmail.com>
2022-06-17 02:34:34 -0700

    Edit script due to betty errors

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
-	{
-		*s1 = *s1 + 1;
-		*s2 = *s2 + 1;
-	}
+	int i = 0;
 
-	if (*s1 == *s2)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i = i + 1;
+	}
 		return (0);
-	}
-	else
-	{
-		return (*s1 - *s2);
-	}
 }
