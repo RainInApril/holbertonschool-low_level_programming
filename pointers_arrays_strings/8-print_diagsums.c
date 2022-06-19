1b78267af97bb910319d04d8a40f8a76f7533a68
root <chaubnphan@gmail.com>
2022-06-18 17:36:52 -0700

    Edit script

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -4,23 +4,25 @@
 /**
  * print_diagsums - prints the sum of the two diagonals of
  * a square matrix of integers
- * @a: matrix
+ * @a: the matrix
  * @size: the size of matrix
  * Return: void
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i, ma1, ma2;
+	int i, j, ma1, ma2;
 
+	i = 0;
+	j = 0;
 	ma1 = 0;
 	ma2 = 0;
-	i = 0;
 
 	while (i < size)
 	{
-		ma1 = ma1 + *(a + i * (size + 1));
-		ma2 = ma2 + *(a + (i + 1) * (size - 1));
+		ma1 = ma1 + *(a + i + j);
+		ma2 = ma2 + *(a - i + j + size - 1);
+		j = j + size;
 	}
 	printf("%d, %d\n", ma1, ma2);
 	i = i + 1;
