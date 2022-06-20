280a7011ce75e56384e80cd1d188a456bddcb257
root <chaubnphan@gmail.com>
2022-06-20 04:15:10 -0700

    Edit betty errors

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * primt_check - checks for prime number
+ * prime_check - checks for prime number
  * @n: the number to be checked
  * @i: the iterator
  * Return: returns 1 if prime, 0 if otherwise
@@ -30,9 +30,9 @@ wsdxcvb nmf, <F12> * otherwise return 0
 
 int is_prime_number(int n)
 {
-        if (n <= 1)
-        {
-                return (0);
-        }
-        return (prime_check(n, n - 1));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (prime_check(n, n - 1));
 }
