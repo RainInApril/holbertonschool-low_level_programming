0dbca3fc20b0a94fee3cd17fc30a7fb6600f0516
root <chaubnphan@gmail.com>
2022-06-21 20:42:16 -0700

    Add a program that prints the number of arguments passed into it

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,3 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 int main(__attribute__((unused))int argc, char *argv[]);
+int main(int argc, char *argv[]__attribute__((unused)));
 
+#endif
