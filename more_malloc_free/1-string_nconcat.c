cf539fd8b1ddae4faec03f81e28046769167bd9e
root <chaubnphan@gmail.com>
2022-06-27 01:06:13 -0700

    Edit script to previous version

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -51,7 +51,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < lens1; i++)
 		ptr[i] = s1[i];
-	for (i = lens1; j < n; i++)
+	for (j = 0; s2[j] != '\0' && j != n; i++)
 	{
 		ptr[i] = s2[j];
 		j++;
