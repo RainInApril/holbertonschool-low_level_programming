3aeb7a916f392152d687cfbafd440d4f9373f59f
root <chaubnphan@gmail.com>
2022-08-18 08:25:11 -0700

    Create a function that gives you the index of a key

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key
+ * @key: the key
+ * @size: the size of the array of the hash table
+ * Return: the index at which the key/value pair should be stored
+ * in the array of the hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value, index;
+
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
+	return (index);
+}
